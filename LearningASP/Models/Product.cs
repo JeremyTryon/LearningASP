0117799c52d9880a05fa38ff3e0a6cee7ed51d54
Jeremy Tryon <germattacks@Gmail.com>
2019-09-26 13:17:32 -0500

    Add project files.

---
+++ LearningASP/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace LearningASP.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set;}
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public string URL { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Product>(this);
+        }
+    }
+}
